3b3526c122982f4b920fc9fe284c401379a8048a
sarr <sarrabdoulaye93@gmail.com>
2020-03-11 01:18:35 +0000

    initialis

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
+import { MenuComponent } from './components/menu/menu.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { PageComponent } from './page/page.component';
+import { ConnexionComponent } from './components/connexion/connexion.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'admin', component: PageComponent},
+  {path: 'connexion', component: ConnexionComponent},
+  { path: '', redirectTo: '/connexion', pathMatch: 'full' },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { PageComponent } from './page/page.component';
+import { GestionUserComponent } from './components/gestion-user/gestion-user.component';
+import { ConnexionComponent } from './components/connexion/connexion.component';
+import { ListUserComponent } from './components/list-user/list-user.component';
+import { ViewUserComponent } from './components/view-user/view-user.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    SidebarComponent,
+    PageComponent,
+    GestionUserComponent,
+    ConnexionComponent,
+    ListUserComponent,
+    ViewUserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+  
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
